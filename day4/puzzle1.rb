9ab47eb5b93adc1fb74eef735ca667fd7d3339d3
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-04 11:14:00 +0100

    Day 4 - Remove unnecessary gsub

--- day4/puzzle1.rb
+++ day4/puzzle1.rb
@@ -9,4 +9,4 @@ end
 puts result
 
 # As a oneliner:
-puts File.read("input.txt", chomp: true).split("\n\n").map { |l| l.gsub("\n", " ") }.count { |line| %w[byr: iyr: eyr: hgt: hcl: ecl: pid:].all? { |d| line.include?(d) } }
+puts File.read("input.txt", chomp: true).split("\n\n").count { |line| %w[byr: iyr: eyr: hgt: hcl: ecl: pid:].all? { |d| line.include?(d) } }
