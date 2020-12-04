9beb679dc0723b9ab2003548024f0358999664f5
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-04 13:10:21 +0100

    Day 4 - remove unnecessary chomp

--- day4/puzzle2.rb
+++ day4/puzzle2.rb
@@ -1,4 +1,4 @@
-input = File.read("input.txt", chomp: true).split("\n\n").map{|l| l.split(/\n|\s/)}
+input = File.read("input.txt").split("\n\n").map{|l| l.split(/\n|\s/)}
 
 conditions = [/^pid:\d{9}$/, /^byr:19[2-9]\d|200[0-2]$/, /^iyr:201[0-9]|2020$/, /^eyr:202[0-9]|2030$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$/ ]
 
@@ -9,4 +9,4 @@ end
 puts result
 
 # As a oneliner:
-puts File.read("input.txt", chomp: true).split("\n\n").map{|l| l.split(/\n|\s/)}.count { |line| [/^pid:\d{9}$/, /^byr:19[2-9]\d|200[0-2]$/, /^iyr:201[0-9]|2020$/, /^eyr:202[0-9]|2030$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$/ ].all? { |c| line.any?{|l| l.match(c)}}}
+puts File.read("input.txt").split("\n\n").map{|l| l.split(/\n|\s/)}.count { |line| [/^pid:\d{9}$/, /^byr:19[2-9]\d|200[0-2]$/, /^iyr:201[0-9]|2020$/, /^eyr:202[0-9]|2030$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$/ ].all? { |c| line.any?{|l| l.match(c)}}}
