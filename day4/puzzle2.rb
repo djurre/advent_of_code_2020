28024316fad5ff48029cd5a99b367ebdcc4c7738
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-04 13:31:48 +0100

    Day 4 - Add missing capture groups

--- day4/puzzle2.rb
+++ day4/puzzle2.rb
@@ -1,6 +1,6 @@
 input = File.read("input.txt").split("\n\n").map{|l| l.split(/\n|\s/)}
 
-conditions = [/^pid:\d{9}$/, /^byr:19[2-9]\d|200[0-2]$/, /^iyr:201[0-9]|2020$/, /^eyr:202[0-9]|2030$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:(1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in$/ ]
+conditions = [/^pid:\d{9}$/, /^byr:(19[2-9]\d|200[0-2])$/, /^iyr:(201[0-9]|2020)$/, /^eyr:(202[0-9]|2030)$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$/]
 
 result = input.count do |line|
   conditions.all? { |c| line.any?{|l| l.match(c)}}
@@ -9,4 +9,4 @@ end
 puts result
 
 # As a oneliner:
-puts File.read("input.txt").split("\n\n").map{|l| l.split(/\n|\s/)}.count { |line| [/^pid:\d{9}$/, /^byr:19[2-9]\d|200[0-2]$/, /^iyr:201[0-9]|2020$/, /^eyr:202[0-9]|2030$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:(1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in$/ ].all? { |c| line.any?{|l| l.match(c)}}}
+puts File.read("input.txt").split("\n\n").map{|l| l.split(/\n|\s/)}.count { |line| [/^pid:\d{9}$/, /^byr:(19[2-9]\d|200[0-2])$/, /^iyr:(201[0-9]|2020)$/, /^eyr:(202[0-9]|2030)$/, /^hcl:#[\da-f]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^hgt:((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$/].all? { |c| line.any?{|l| l.match(c)}}}
