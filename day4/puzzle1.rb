5740fcb559859ee4016ff3b4f04f7524f1896f39
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-04 11:12:37 +0100

    Add solution for day 4 - part 1

---
+++ day4/puzzle1.rb
@@ -0,0 +1,12 @@
+input = File.read("input.txt", chomp: true).split("\n\n").map { |l| l.gsub("\n", " ") }
+
+mandatory = %w[byr: iyr: eyr: hgt: hcl: ecl: pid:]
+
+result = input.count do |line|
+  mandatory.all? { |d| line.include?(d) }
+end
+
+puts result
+
+# As a oneliner:
+puts File.read("input.txt", chomp: true).split("\n\n").map { |l| l.gsub("\n", " ") }.count { |line| %w[byr: iyr: eyr: hgt: hcl: ecl: pid:].all? { |d| line.include?(d) } }
