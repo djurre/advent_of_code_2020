e3050306ca526f57e21cc3261031b6ecac66ac70
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-07 21:40:43 +0100

    Day 7 part 1 - Add a one-liner

--- day7/puzzle1.rb
+++ day7/puzzle1.rb
@@ -6,3 +6,7 @@ def find(color)
 end
 
 puts find("shiny gold").flatten.uniq.count
+
+# As a one-liner
+puts (process = lambda { |color| File.readlines("input.txt").filter_map { |l| l.match(/^(.*) bags+ contain.*#{Regexp.quote(color)}/)&.captures }.flatten.map { |b| [b] + process.call(b) } }).call("shiny gold").flatten.uniq.count
+
