1f39014c212873d805debbe99ae8d6713457a578
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-10 18:21:04 +0100

    Day 10 part 2 - Add graph theoretic solution

--- day10/puzzle2.rb
+++ day10/puzzle2.rb
@@ -1,5 +1,6 @@
 input = File.readlines("input.txt", chomp: true).map(&:to_i)
 
+# My original way
 pp ([0] + input.sort + [input.max + 3]).
   sort.
   each_cons(3).
@@ -7,3 +8,13 @@ pp ([0] + input.sort + [input.max + 3]).
   slice_when { |prev, curr| curr != prev.next }.to_a.
   map { |group| (0..2 ** group.size).count { |mask| ('%03b' % mask).count("0") <= 2 } }.
   inject(:*)
+
+
+# The graph way
+cache = {0 => 1}
+cache.default = 0
+(input + [input.max + 3]).sort.each do |num|
+  cache[num] = (1..3).sum { |t| cache[num - t]  }
+end
+
+pp cache[input.max]
