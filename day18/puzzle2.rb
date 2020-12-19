29b8eaa628544a90640832d22b2e038393106707
Djurre Broekhuis <djurre.broekhuis@nedap.com>
2020-12-19 17:40:30 +0100

    Day 18 - Add regex solution

--- day18/puzzle2.rb
+++ day18/puzzle2.rb
@@ -1,7 +1,32 @@
+# The kind of cheating way
 class Integer
   def **(op)
     self + op
   end
 end
-
 puts File.readlines("input.txt", chomp: true).sum { |line| eval(line.gsub("+", "**")) }
+
+#The naughty regex way
+input = File.readlines("input.txt", chomp: true).map { |line| line.gsub(" ", "") }
+REGEX = /\(((?:\d+|\+|\*)+)\)/
+
+def calculate(line, op)
+  while line.include?(op)
+    line.gsub!(/(\d+(?:#{Regexp.escape(op)})\d+)/) { |r| eval(r).to_s}
+  end
+  line
+end
+
+res = input.map do |line|
+  while line.include?("(")
+    line.gsub!(REGEX) { |m| calculate(m, "+") }
+    line.gsub!(REGEX) { |m| calculate(m, "*") }
+    line.gsub!(/\((\d+)\)/, '\1')
+  end
+  line = calculate(line, "+")
+  line = calculate(line, "*")
+  line
+end
+
+pp res.map(&:to_i).sum
+
